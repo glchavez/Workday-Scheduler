1654f1f54b6c56b411eb7f9512a2abed0e47384c
Giovany <giochavez320@gmail.com>
2021-02-27 18:29:10 -0700

    Additon of military time conversion and local storage reset js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,15 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 
+function updatePage() {
+
+    // Variable equal to the current hour in military time
+    var currentTime = parseInt(moment().hours());
+
+    // Reset local storage at end of work day, 5 pm
+    if (currentTime > 17) {
+        localStorage.clear();
+    }
+
+
+};
