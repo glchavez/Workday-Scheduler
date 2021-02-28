c917ff7c59fa140cdee1245a0c516dfe1c34976b
Giovany <giochavez320@gmail.com>
2021-02-27 18:30:03 -0700

    Addition of local storage displayal in time slot js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,5 +12,17 @@ function updatePage() {
         localStorage.clear();
     }
 
+    // Display local storage content in each hour slot, even with a page refresh
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));    
 
+
+    
 };
